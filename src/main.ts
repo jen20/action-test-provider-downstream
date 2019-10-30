5c115ccfbf7903c260075f1fb0b235dbe2462b93
James Nugent <james@jen20.com>
2019-10-30 10:26:50 +0100

    Write commit SHA to debug log

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,7 @@ async function find_commit_sha(path: string, offset: number = 0): Promise<string
 async function run() {
     try {
         const checkoutSHA = process.env.GITHUB_SHA;
+        core.debug(`Checkout SHA: ${checkoutSHA}`)
         const branchName = `integration/pulumi-terraform-bridge/${checkoutSHA}`;
 
         // Ensure that the bot token is masked in the log output
